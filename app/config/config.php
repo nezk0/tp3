f1b7c51691058f8f3b8233f70e52995fa19ba5c6
Pierre Weil <21705713@campus.unicaen.fr>
2018-05-18 13:42:13 +0200

    first commit

---
+++ app/config/config.php
@@ -0,0 +1,30 @@
+<?php
+return array(
+		"siteUrl"=>"http://127.0.0.1/tp3-pw/",
+		"database"=>[
+				"type"=>"mysql",
+				"dbName"=>"",
+				"serverName"=>"127.0.0.1",
+				"port"=>"3306",
+				"user"=>"root",
+				"password"=>"",
+				"options"=>[],
+				"cache"=>false
+		],
+		"sessionName"=>"tp3-pw",
+		"namespaces"=>[],
+		"templateEngine"=>'Ubiquity\\views\\engine\\Twig',
+		"templateEngineOptions"=>array("cache"=>false),
+		"test"=>false,
+		"debug"=>false,
+		"di"=>["jquery"=>function($controller){
+							$jquery=new \Ajax\php\ubiquity\JsUtils(["defer"=>true],$controller);
+							$jquery->semantic(new \Ajax\Semantic());
+							return $jquery;
+						}],
+		"cache"=>["directory"=>"cache/","system"=>"Ubiquity\\cache\\system\\ArrayCache","params"=>[]],
+		"mvcNS"=>["models"=>"models","controllers"=>"controllers","rest"=>""],
+		"isRest"=>function(){
+			return \Ubiquity\utils\http\URequest::getUrlParts()[0]==="rest";
+		}
+);
